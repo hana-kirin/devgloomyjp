cbeb13dfb695ff7bc6bdcb8f0a2a7d8d1b223818
hana-kirin <inadanaoki@gmail.com>
2026-08-29 11:53:37 +0900

    タイトル確認中

--- front-page.php
+++ front-page.php
@@ -38,7 +38,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>馬のいななき | UMANO INANAKI</title>
-<meta name="description" content="馬のいななき オフィシャルサイト">
+<meta name="description" content="馬のいななき">
 <style>
 :root{
   --paper:#f2f1ee; --panel:#e9e8e4;
